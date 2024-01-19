234b99bd668ab1e30be0bce29b26bd8b2367868b
Kaketo Shimizu <ipkake0527@yahoo.co.jp>
2024-01-19 12:01:53 +0900

    [add] implement conv1 layer

---
+++ Brain/capsule_network.py
@@ -0,0 +1,34 @@
+#
+# Dynamic Routing Between Capsules
+# https://arxiv.org/pdf/1710.09829.pdf
+#
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+import torch.nn.functional as F
+
+
+class Conv1(nn.Module):
+	def __init__(self):
+		super(Conv1, self).__init__()
+
+		self.conv = nn.Conv2d(
+			in_channels=1,
+			out_channels=16,
+			kernel_size=3,
+			stride=4,
+			bias=True
+		)
+
+		self.relu = nn.ReLU(inplace=True)
+
+	def forward(self, x):
+		# x: [batch_size, 1, 84, 84]
+
+		h = self.relu(self.conv(x))
+		# h: [batch_size, 16, 21, 21]
+
+		return h
